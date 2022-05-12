6f6301aa4cdf68899d19407e9662610426f2e604
sapnajariya <jariyasapna@gmail.com>
2022-05-12 23:24:32 +0530

    exam

---
+++ TableperClass/src/in/co/rays/bean/Cheque.java
@@ -0,0 +1,14 @@
+package in.co.rays.bean;
+
+public class Cheque extends Payment {
+	private String Chequetype;
+
+	public String getChequetype() {
+		return Chequetype;
+	}
+
+	public void setChequetype(String chequetype) {
+		Chequetype = chequetype;
+	}
+
+}
---
+++ tableperconcret/src/in/co/rays/bean/Test.java
@@ -0,0 +1,36 @@
+package in.co.rays.bean;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Test {
+	public static void main(String[] args) {
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+		Session session=sf.openSession();
+		Transaction tx=session.beginTransaction();
+	
+		CreditCard c=new CreditCard();
+
+        c.setPaymentId(10);
+        c.setAmount(2500);
+        c.setCreditCardType("Visa");
+
+        Cheque c1=new Cheque();
+
+        c1.setPaymentId(11);
+        c1.setAmount(2600);
+        c1.setChequetype("ICICI");
+
+        
+        session.save(c);
+        session.save(c1);
+        System.out.println("Object saved successfully.....!!");
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
+
+
